5c57b672efd63e9bfda5939ca5119e0652c4f110
Victor Muia <victor.muia@storyful.com>
2018-06-21 16:47:08 +0100

    Added test eventbrite python query

---
+++ eventbrite.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+##
+## Script pulls stats from eventbrite sessions
+##
+
+import httplib
+import os
+import urllib
+
+if __name__ == "__main__":
+
+    token = os.environ['EVENTBRITE_TOKEN']
+    params = '?token=' + token
+    url = 'www.eventbriteapi.com'
+    service = '/v3/users/me/'
+
+    conn = httplib.HTTPSConnection(url , httplib.HTTPS_PORT)
+    conn.request("POST", service + params)
+    response = conn.getresponse()
+    print 'response', response.status, response.reason
+
+    data = response.read()
+    print 'data', data
+    conn.close()
