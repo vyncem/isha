76a4750d4602f339b734915e47954beb8280b4b8
Victor Muia <victor.muia@storyful.com>
2018-11-16 17:00:03 +0000

    Send mail stats

---
+++ sendmail.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'net/smtp'
+
+RECEPIENTS=['vyncem@gmail.com', 'offering.europe@ishafoundation.org', 'ishaktijaiswal13@gmail.com']
+
+message = <<MESSAGE_END
+From: Automation  <automation@achillesmail.com>
+To: Isha Offering <offering.europe@ishafoundation.org>
+MIME-Version: 1.0
+Content-type: text/html
+Subject: Offering Automation report
+
+MESSAGE_END
+
+File.open("#{ENV['FREE_OFFERING_WORKSPACE']}/.email", "r") do |f|
+  f.each_line do |line|
+    message << line
+  end
+end
+Net::SMTP.start('localhost') do |smtp|
+   smtp.send_message message, 'automation@achillesmail.com', RECEPIENTS
+end
